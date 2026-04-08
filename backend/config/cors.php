ee146422381aff9aaa8ff5600ff7013c8893769c
Lucas D Candido <lucasdcandido1994@gmail.com>
2026-04-08 10:12:28 -0300

    Fix CORS config for Railway

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,19 +1,24 @@
 <?php
 
+$frontendUrl = env('FRONTEND_URL', 'http://localhost:3000');
+
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [
+        $frontendUrl,
+        'https://outstanding-luck-production-1bba.up.railway.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['*', 'Authorization', 'Content-Type', 'Accept'],
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];
